7dc0eb0190cd6b19dc8115e95d9bdeae236bd5e3
RonnieHoffer <ronnie.hoffer@gmail.com>
2021-01-18 23:51:51 -0500

    Contact book now functional

--- asgn010.rb
+++ asgn010.rb
@@ -1,8 +1,7 @@
 require_relative 'contact'
 require_relative 'crm'
 
-alpha = Contact.create("Ronnie", "Hoffer", "ronnie.hoffer@gmail.com", "Great trumpet player")
-beta = Contact.create("Darryl", "Hoffer", "darryl.hoffer@gmail.com", "Great hockey player")
-theta = Contact.create("Straw", "Man", "strawman@purplefuzz.com", "He is made out of straw")
+alpha = Contact.create("Ronnie", "Hoffer", "ronnie.hoffer@gmail.com", "Trumpet player")
+beta = Contact.create("Straw", "Man", "strawman@purplefuzz.com", "He is made out of straw")
 
 CRM.new
